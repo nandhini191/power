6da299b2af3d13c853578dea6492d7784e49fbad
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-02-22 16:30:15 +0530

    Create yes.c

---
+++ yes.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    char a[20];
+    int i,count=0;
+    printf("\n enter the string");
+    scanf("%s",&a[i]);
+    for(i=0;i<=a[i];i++)
+    {
+        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+    {
+        count++;
+        if(count>0)
+        {
+            printf("\n yes");
+        }
+        else
+        {
+            printf("\n no");
+        }
+    }
+    }
+    }
