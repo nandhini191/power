f775778ab0d4de09532509851700d9717bbd500b
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-02-27 16:51:23 +0530

    Create perfect.c

---
+++ perfect.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a*b;
+ if(a==b)
+    {
+        
+        printf("\n perfect square");
+    }
+        else
+        {
+        printf("\n not a perfect square");
+    }
+}
