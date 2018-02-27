f7a04657e2f497929e3786c6ceb6e33ea2f0ab72
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-02-27 15:43:16 +0530

    Create decimal.c

---
+++ decimal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+    {
+        float n;
+        int a;
+        printf("\n enter the n value"); 
+        scanf("%f",&n);
+        n++;
+        a=n;
+        printf("\n print integer value is %d",a);
+        return 0;
+    }
