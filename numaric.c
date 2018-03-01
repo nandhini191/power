c86bd4972402de053813bd1ec6de40b918885ec8
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-03-01 15:55:55 +0530

    Create numaric.c

---
+++ numaric.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char a[20];
+   int i;
+   printf("\n enter the character");
+   scanf("%s",&a);
+   for(i=0;i<=a[i];i++)
+   {
+       if(a[i]>='0'&&a[i]<='9')
+       
+       printf("\n %c",a[i]);
+   }
+   return 0;
+   }
+
