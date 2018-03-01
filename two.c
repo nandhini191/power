f1513fed3afcfd7a5bb36f630b8654a27fd91f30
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-03-01 16:26:30 +0530

    Create two.c

---
+++ two.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[20];
+    int i,j;
+    printf("\n enter the number");
+    scanf("%s",&a);
+    for(i=0;i<=a[i];i++)
+    {
+        for(j=0;j<=a[i];j++)
+        {
+         
+        }}
+            if(a[i]==a[j])
+            printf("no");
+            else
+            printf("yes");
+        
+    return 0;
+}
