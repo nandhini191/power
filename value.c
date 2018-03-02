d58b4fcf2565d7143d524f2e50a47cc59620325e
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-03-02 14:19:16 +0530

    Create value.c

---
+++ value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[20];
+    int i,count,l;
+    scanf("%s",&a);
+    l=strlen(a);
+    for(i=0;a[i]<=l;i++)
+    {
+       } if(a[i]=='\0')
+        
+        count++;
+        
+    printf("%d",++count);
+    return 0;
+}
