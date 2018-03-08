a0d17da837b1cef6f71b979e93c43222e525ce03
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-03-08 16:12:19 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],max,i,j;
+    
+   
+    for(i=0;i<=10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    a[0]=max;
+    for(j=0;j<=10;j++);
+    {
+        if(a[i]>max)
+       
+            max=a[i];
+        
+    }    
+    printf("%d",max);
+    return 0;
+}
