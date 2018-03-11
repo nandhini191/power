5829f1e899369bd9e45f68cbbcaaf1181d68e129
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-03-11 11:04:51 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("\n enter the n value");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
