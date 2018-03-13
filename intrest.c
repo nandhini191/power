d0f880992c3614480976339070c4f3431363d3ec
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-03-13 16:49:41 +0530

    Create intrest.c

---
+++ intrest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int p,n,r,intrest;
+    printf("\n enter values of p, n,r");
+    scanf("%d%d%d",&p,&n,&r);
+    intrest=(p*n*r)/100;
+    printf("\n intrest is%d",intrest);
+}
