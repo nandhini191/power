ab4505f0ed07596348b59e5f9b711983c52ab41c
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-03-20 16:39:23 +0530

    Create float.c

---
+++ float.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float length,breadth;
+    double area;
+    printf("%f%f",&length,&breadth);
+    area=length*breadth;
+    printf("\n area is %lf",area);
+return 0;
+}
